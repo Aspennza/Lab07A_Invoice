89cd3bc27971eaf35a7d55c8b85dabcdc7af1634
Zoe Aspenns <aspennza@mail.uc.edu>
2025-10-15 18:56:30 -0400

    Created LineItem class draft

---
+++ src/LineItem.java
@@ -0,0 +1,55 @@
+import java.util.Objects;
+
+public class LineItem
+{
+    private double quantity;
+    private double calculatedTotal;
+    private Product userProduct;
+
+    public LineItem(double quantity, double calculatedTotal, Product userProduct)
+    {
+        this.quantity = quantity;
+        this.calculatedTotal = calculatedTotal;
+        this.userProduct = userProduct;
+    }
+
+    public double getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(double quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getCalculatedTotal() {
+        return calculatedTotal;
+    }
+
+    public Product getUserProduct() {
+        return userProduct;
+    }
+
+    //May not need toString??
+    @Override
+    public String toString() {
+        return "LineItem{" +
+                "quantity=" + quantity +
+                ", calculatedTotal=" + calculatedTotal +
+                ", userProduct=" + userProduct +
+                '}';
+    }
+
+    //May not need?
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        LineItem lineItem = (LineItem) o;
+        return Double.compare(quantity, lineItem.quantity) == 0 && Double.compare(calculatedTotal, lineItem.calculatedTotal) == 0 && Objects.equals(userProduct, lineItem.userProduct);
+    }
+
+    //May not need?
+    @Override
+    public int hashCode() {
+        return Objects.hash(quantity, calculatedTotal, userProduct);
+    }
+}

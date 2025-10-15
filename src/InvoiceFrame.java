53df35acd8ce2af0c42d0a6f55c3d6f3cf47fac4
Zoe Aspenns <aspennza@mail.uc.edu>
2025-10-15 18:14:09 -0400

    Drafted createBillingInfoPnl()

--- src/InvoiceFrame.java
+++ src/InvoiceFrame.java
@@ -14,8 +14,9 @@ public class InvoiceFrame extends javax.swing.JFrame
     JLabel titleLbl;
     Font titlePnlFont;
 
+    Font subtitleFont = new Font("Serif", Font.BOLD, 25);
+
     JLabel prodInfoLbl;
-    Font prodInfoFont;
     JLabel prodNameLbl;
     JLabel unitPriceLbl;
     JLabel quantityLbl;
@@ -44,6 +45,18 @@ public class InvoiceFrame extends javax.swing.JFrame
 
     GridBagConstraints gbc = new GridBagConstraints();
 
+    GridBagConstraints titleGBC = new GridBagConstraints();
+    GridBagConstraints label1GBC = new GridBagConstraints();
+    GridBagConstraints TF1GBC = new GridBagConstraints();
+    GridBagConstraints label2GBC = new GridBagConstraints();
+    GridBagConstraints TF2GBC = new GridBagConstraints();
+    GridBagConstraints label3GBC = new GridBagConstraints();
+    GridBagConstraints TF3GBC = new GridBagConstraints();
+    GridBagConstraints label4GBC = new GridBagConstraints();
+    GridBagConstraints TF4GBC = new GridBagConstraints();
+    GridBagConstraints label5GBC = new GridBagConstraints();
+    GridBagConstraints TF5GBC = new GridBagConstraints();
+
     public InvoiceFrame()
     {
         super("Invoice Creator");
@@ -60,6 +73,8 @@ public class InvoiceFrame extends javax.swing.JFrame
         createTitlePnl();
 
         createProductInfoPnl();
+
+        createBillingInfoPnl();
 
         setSize(screenWidth * 3/4, screenHeight * 3/4);
         setLocationRelativeTo(null);
@@ -96,20 +111,12 @@ public class InvoiceFrame extends javax.swing.JFrame
         gbc.gridheight = 1;
         gbc.fill = GridBagConstraints.BOTH;
 
-        GridBagConstraints titleGBC = new GridBagConstraints();
-        GridBagConstraints label1GBC = new GridBagConstraints();
-        GridBagConstraints TF1GBC = new GridBagConstraints();
-        GridBagConstraints label2GBC = new GridBagConstraints();
-        GridBagConstraints TF2GBC = new GridBagConstraints();
-        GridBagConstraints label3GBC = new GridBagConstraints();
-        GridBagConstraints TF3GBC = new GridBagConstraints();
-
         productInfoPnl.setBorder(new EtchedBorder());
         productInfoPnl.setLayout(new GridBagLayout());
 
         prodInfoLbl = new JLabel("Product Information");
-        prodInfoFont = new Font("Serif", Font.BOLD, 25);
-        prodInfoLbl.setFont(prodInfoFont);
+
+        prodInfoLbl.setFont(subtitleFont);
         titleGBC.gridx = 0;
         titleGBC.gridy = 0;
         titleGBC.gridwidth = 2;
@@ -165,5 +172,110 @@ public class InvoiceFrame extends javax.swing.JFrame
         productInfoPnl.add(quantityLbl, label3GBC);
         productInfoPnl.add(quantityTF, TF3GBC);
         mainPnl.add(productInfoPnl, gbc);
+    }
+
+    private void createBillingInfoPnl()
+    {
+        billingInfoPnl = new JPanel();
+        gbc.gridx = 1;
+        gbc.gridy = 1;
+        gbc.gridwidth = 1;
+        gbc.gridheight = 1;
+        gbc.fill = GridBagConstraints.BOTH;
+
+        billingInfoPnl.setBorder(new EtchedBorder());
+        billingInfoPnl.setLayout(new GridBagLayout());
+
+        billingInfoLbl = new JLabel("Billing Information");
+        billingInfoLbl.setFont(subtitleFont);
+        titleGBC.gridx = 0;
+        titleGBC.gridy = 0;
+        titleGBC.gridwidth = 2;
+        titleGBC.gridheight = 1;
+        titleGBC.fill = GridBagConstraints.BOTH;
+
+        billingInfoPnl.add(billingInfoLbl, titleGBC);
+
+        companyLbl = new JLabel("Company to be billed:");
+        label1GBC.gridx = 0;
+        label1GBC.gridy = 1;
+        label1GBC.gridwidth = 1;
+        label1GBC.gridheight = 1;
+        label1GBC.fill = GridBagConstraints.BOTH;
+        companyTF = new JTextField(15);
+        TF1GBC.gridx = 1;
+        TF1GBC.gridy = 1;
+        TF1GBC.gridwidth = 1;
+        TF1GBC.gridheight = 1;
+        TF1GBC.fill = GridBagConstraints.BOTH;
+
+        billingInfoPnl.add(companyLbl, label1GBC);
+        billingInfoPnl.add(companyTF, TF1GBC);
+
+        streetLbl = new JLabel("Street address:");
+        label2GBC.gridx = 0;
+        label2GBC.gridy = 2;
+        label2GBC.gridwidth = 1;
+        label2GBC.gridheight = 1;
+        label2GBC.fill = GridBagConstraints.BOTH;
+        streetTF = new JTextField(15);
+        TF2GBC.gridx = 1;
+        TF2GBC.gridy = 2;
+        TF2GBC.gridwidth = 1;
+        TF2GBC.gridheight = 1;
+        TF2GBC.fill = GridBagConstraints.BOTH;
+
+        billingInfoPnl.add(streetLbl, label2GBC);
+        billingInfoPnl.add(streetTF, TF2GBC);
+
+        cityLbl = new JLabel("City:");
+        label3GBC.gridx = 0;
+        label3GBC.gridy = 3;
+        label3GBC.gridwidth = 1;
+        label3GBC.gridheight = 1;
+        label3GBC.fill = GridBagConstraints.BOTH;
+        cityTF = new JTextField(15);
+        TF3GBC.gridx = 1;
+        TF3GBC.gridy = 3;
+        TF3GBC.gridwidth = 1;
+        TF3GBC.gridheight = 1;
+        TF3GBC.fill = GridBagConstraints.BOTH;
+
+        billingInfoPnl.add(cityLbl, label3GBC);
+        billingInfoPnl.add(cityTF, TF3GBC);
+
+        stateLbl = new JLabel("State:");
+        label4GBC.gridx = 0;
+        label4GBC.gridy = 4;
+        label4GBC.gridwidth = 1;
+        label4GBC.gridheight = 1;
+        label4GBC.fill = GridBagConstraints.BOTH;
+        stateTF = new JTextField(15);
+        TF4GBC.gridx = 1;
+        TF4GBC.gridy = 4;
+        TF4GBC.gridwidth = 1;
+        TF4GBC.gridheight = 1;
+        TF4GBC.fill = GridBagConstraints.BOTH;
+
+        billingInfoPnl.add(stateLbl, label4GBC);
+        billingInfoPnl.add(stateTF, TF4GBC);
+
+        zipLbl = new JLabel("Zip Code:");
+        label5GBC.gridx = 0;
+        label5GBC.gridy = 5;
+        label5GBC.gridwidth = 1;
+        label5GBC.gridheight = 1;
+        label5GBC.fill = GridBagConstraints.BOTH;
+        zipTF = new JTextField(15);
+        TF5GBC.gridx = 1;
+        TF5GBC.gridy = 5;
+        TF5GBC.gridwidth = 1;
+        TF5GBC.gridheight = 1;
+        TF5GBC.fill = GridBagConstraints.BOTH;
+
+        billingInfoPnl.add(zipLbl, label5GBC);
+        billingInfoPnl.add(zipTF, TF5GBC);
+
+        mainPnl.add(billingInfoPnl, gbc);
     }
 }
